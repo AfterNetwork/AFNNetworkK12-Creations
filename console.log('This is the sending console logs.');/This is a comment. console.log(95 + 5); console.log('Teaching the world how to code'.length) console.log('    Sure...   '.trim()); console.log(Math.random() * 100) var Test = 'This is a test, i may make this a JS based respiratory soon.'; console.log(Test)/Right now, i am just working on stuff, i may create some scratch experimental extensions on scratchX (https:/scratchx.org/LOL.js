b647574cd00357178d63bfbfc4e91bd47e531239
After <45772308+AfterNetwork@users.noreply.github.com>
2019-03-06 09:53:24 -0800

    Create LOL.js

---
+++ console.log('This is the sending console logs.');/This is a comment. console.log(95 + 5); console.log('Teaching the world how to code'.length) console.log('    Sure...   '.trim()); console.log(Math.random() * 100) var Test = 'This is a test, i may make this a JS based respiratory soon.'; console.log(Test)/Right now, i am just working on stuff, i may create some scratch experimental extensions on scratchX (https:/scratchx.org/LOL.js
@@ -0,0 +1,8 @@
+console.log('This is the sending console logs.');
+// This is a comment.
+console.log(95 + 5);
+console.log('Teaching the world how to code'.length)
+console.log('    Sure...   '.trim());
+console.log(Math.random() * 100)
+console.log(Test)
+
