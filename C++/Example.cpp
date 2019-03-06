4c806fc8185830f647dde8d0a81d4e78786c738e
After <45772308+AfterNetwork@users.noreply.github.com>
2019-03-06 09:34:25 -0800

    Update Example.cpp

--- C++/Example.cpp
+++ C++/Example.cpp
@@ -1,2 +1,11 @@
 std::cout << "Sure, Why Not?\n";
 std::cout << "This is a sampe for C++.\n";
+int Sample;
+Sample = 500;
+int C++ = 10;
+int Hello;
+Hello = 800 * 200;
+std::cout << Hello << "\n";
+int Money = 1;
+std::cout << "Enter Money Amount";
+std::cin >> Money;
