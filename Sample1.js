e912bf2db668fdd1b0dafe0ad91f6759217cc731
After <45772308+AfterNetwork@users.noreply.github.com>
2019-03-06 09:55:47 -0800

    Create Sample1.js

---
+++ Sample1.js
@@ -0,0 +1,10 @@
+console.log('This is the sending console logs.');
+// This is a comment.
+console.log(95 + 5);
+console.log('Teaching the world how to code'.length)
+console.log('    Sure...   '.trim());
+console.log(Math.random() * 100)
+var Test = 'This is a test, i may make this a JS based respiratory soon.';
+console.log(Test)
+const Job = 'Unemployed';
+console.log(Job);
