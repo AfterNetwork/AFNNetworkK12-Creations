7f6fbe3d22f8fefbca63e1e3b481ca0dd8eb3a7b
After <45772308+AfterNetwork@users.noreply.github.com>
2019-03-06 09:39:23 -0800

    Create Ruby.rb

---
+++ Ruby/Ruby.rb
@@ -0,0 +1,9 @@
+Ruby_Sample = "Ok, this is a sample, i don't care what you say about this."
+
+Ruby_Boolean_Sample = true
+
+Ruby_Number = 55
+
+puts Ruby_Sample
+puts Ruby_Boolean_Sample
+puts Ruby_Number
