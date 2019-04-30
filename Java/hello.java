05754ddb4c826f7f79401df7a4c16a6a60a9eac2
After <45772308+AfterNetwork@users.noreply.github.com>
2019-04-30 11:31:42 -0700

    Update hello.java

--- Java/hello.java
+++ Java/hello.java
@@ -1,11 +1,11 @@
-import java.util.scanner;
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    Scanner github = new Scanner(System.in);
-    int XP = 50
-    System.out.println("Type in how much XP you want, you have currently have 50 XP")
-      int XP +=  keyboard.next();
-      
-       
+    Scanner keyboard = new Scanner(System.in);
+    int XP = 50;
+    System.out.println("Type in how much XP you want, you have currently have 50 XP");
+       XP +=  keyboard.nextInt();
+      System.out.println("You have " + XP + " XP.");
+        keyboard.close(); 
   }
 }
