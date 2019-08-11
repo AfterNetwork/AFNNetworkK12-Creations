8e5e9a15922fa1629092c1e05f2617c45202044f
After <45772308+AfterNetwork@users.noreply.github.com>
2019-08-10 19:49:04 -0700

    Random numben generator

--- Javascript/numbers.js
+++ Javascript/numbers.js
@@ -1,1 +1,8 @@
-let 
+let thing = 0;
+function NetworkJS.random(number) {
+  for (thing != number) {
+    thing = Math.floor(Math.random * number);
+    return thing
+  }
+}
+console.log(NetworkJS.random(50));
