37271e1afbf15ca5c08b2024a9b9ed7aae7dc4df
After <45772308+AfterNetwork@users.noreply.github.com>
2019-09-11 11:05:32 -0700

    Update infinite.js

--- JavaScript-NVs/infinite.js
+++ JavaScript-NVs/infinite.js
@@ -1,1 +1,13 @@
+while (true) {
+  console.log("bad word" + "f***");
+  console.log("a b c d e f g u n");
+  console.log("lol");
 
+}
+while (5 + 1 == true) {
+  console.log("9/11 on the 2001 channel")
+}
+console.log("bad fart bad fart")
+// you will never see this secret message from the developers:
+// secret message
+console.log("a b c d e f g u n");
